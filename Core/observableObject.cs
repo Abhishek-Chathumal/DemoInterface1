11a56c1237420095cfaf2b65a45e8a00a7665454
Abhishek Wellage <codse192f-055@student.nibm.lk>
2021-07-12 12:19:31 +0530

    Add project files.

---
+++ Core/observableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace DemoInterface1.Core
+{
+    class observableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ MVVM/ModelView/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using DemoInterface1.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoInterface1.MVVM.ModelView
+{
+    class MainViewModel:observableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand VehicleViewCommand { get; set; }
+        public RelayCommand CustomerViewCommand { get; set; }
+        public RelayCommand OwnerViewCommand { get; set; }
+
+        public HomeViewModel HomeVM {get; set;}
+        public VehicleViewModel VehicleVM { get; set;}
+        public CustomerViewModel CustomerVM { get; set;}
+        public OwnerViewModel OwnerVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+            
+
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            VehicleVM = new VehicleViewModel();
+            CustomerVM = new CustomerViewModel();
+            OwnerVM = new OwnerViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVM;
+            });
+            VehicleViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = VehicleVM;
+            });
+            CustomerViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CustomerVM;
+            });
+            OwnerViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = OwnerVM;
+            });
+        }
+    }
+}
