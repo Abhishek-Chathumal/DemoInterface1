2abfb773c706c0cc7fd4ed52a4eb998a7b9ec606
Abhishek Wellage <codse192f-055@student.nibm.lk>
2021-07-12 20:13:18 +0530

    Added Functionality: Open a new User Control From the Current user Control

--- MVVM/ModelView/AddCustomerViewModel.cs
+++ MVVM/ModelView/AddCustomerViewModel.cs
@@ -1,5 +1,4 @@
-﻿using DemoInterface1.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DemoInterface1.MVVM.ModelView
 {
-    class CustomerViewModel
+    class AddCustomerViewModel
     {
     }
 }
